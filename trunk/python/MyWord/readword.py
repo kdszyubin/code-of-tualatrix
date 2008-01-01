1480206e2a99dc69f848d3d9a8706ec4b9f60322
tualatrix <tualatrix@bcf3a914-dc41-0410-a79b-5dc8c4d4b3d5>
2008-01-01 12:01:34 +0000

    =

--- trunk/python/MyWord/readword.py
+++ trunk/python/MyWord/readword.py
@@ -4,11 +4,16 @@ import os
 import sys
 import subprocess
 
-def readword(word = None, type = None):
-	if type:
-		subprocess.Popen(["aplay", "/usr/share/reciteword/modules/type.wav"])
-	else:
+def readword(word):
+	if word:
 		subprocess.Popen(["aplay","/usr/share/WyabdcRealPeopleTTS/%s/%s.wav" % (word[0], word)])
 
+def typeword():
+	subprocess.Popen(["aplay","/usr/share/reciteword/modules/type.wav"])
+
+def delword():
+	subprocess.Popen(["aplay","/usr/share/reciteword/modules/back.wav"])
+	
+	
 if __name__ == "__main__":
 	readword(sys.argv[1])
