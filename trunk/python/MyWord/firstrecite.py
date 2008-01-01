c6b756b47e5e611478095565be6a7003595eb63f
tualatrix <tualatrix@bcf3a914-dc41-0410-a79b-5dc8c4d4b3d5>
2008-01-01 09:39:33 +0000

    =

--- trunk/python/MyWord/firstrecite.py
+++ trunk/python/MyWord/firstrecite.py
@@ -4,6 +4,7 @@
 import os
 import gtk
 import datetime 
+import gobject
 import time
 import cPickle as pickle
 from dictfile import DictFile
@@ -57,59 +58,93 @@ class ReciteRecord:
 		 return "".join(["%s\t%s" % (word, self.dict[word]) for word in self.words])
 
 class FirstRecite(gtk.VBox):
-	def __init__(self, book = None):
+	def __init__(self, book):
 		gtk.VBox.__init__(self)
-		if book:
-			self.book = book
-			self.create_words_list()
+		self.book = book
+		self.show()
 
-	def create_words_list(self):
-		self.hpaned = gtk.HPaned()
-		self.pack_start(self.hpaned)
+		# Stage 1, Confirm WordList
+		self.wordlist = self.create_wordlist()
+		self.pack_start(self.wordlist)
+
+		# Stage 2, Word Preview
+		self.preview = self.create_wordpreview()
+		self.pack_start(self.preview)
+		
+	def create_wordlist(self):
+		hpaned = gtk.HPaned()
+		hpaned.show()
 
 		sw = gtk.ScrolledWindow()
+		sw.show()
 		sw.set_size_request(200, -1)
 		sw.set_shadow_type(gtk.SHADOW_ETCHED_IN)
 		sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
 
-		textview, self.rr = self.create_textview()
-		sw.add(textview)
+		listview, self.rr = self.create_listview()
+		sw.add(listview)
 
-		self.hpaned.pack1(sw)
+		hpaned.pack1(sw)
 
 		vbox = gtk.VBox(False, 10)
+		vbox.show()
+
 		button = gtk.Button("确定")
+		button.show()
 		button.connect("clicked", self.button_clicked_cb)
 		vbox.pack_end(button, False, False, 0)
 
 		button = gtk.Button("返回")
+		button.show()
 		vbox.pack_end(button, False, False, 0)
 
-		self.hpaned.pack2(vbox)
+		hpaned.pack2(vbox)
 
-	def button_clicked_cb(self, widget, data = None):
-		self.hpaned.destroy()
-		self.create_wordpreview()
+		return hpaned
 
 	def create_wordpreview(self):
-		en = gtk.Label("")
-		self.pack_start(en)
-		cn = gtk.Label("")
-		self.pack_start(cn)
-		for word in self.rr.words:
-			en.set_text(word)
-			cn.set_text(self.rr.dict[word])
+		vbox = gtk.VBox(False, 0)
 
-	def create_textview(self):
-		textview = gtk.TextView()
-		buffer = gtk.TextBuffer()
-		
+		en = gtk.Label("EN")
+		en.show()
+		vbox.pack_start(en)
+		cn = gtk.Label("CN")
+		cn.show()
+		vbox.pack_start(cn)
+
+		return vbox
+
+	def button_clicked_cb(self, widget, data = None):
+		self.wordlist.hide()
+		self.preview.show()
+
+	def create_listview(self):
+		listview = gtk.TreeView()
+		listview.show()
+
+		model = gtk.ListStore(
+				gobject.TYPE_STRING,
+				gobject.TYPE_STRING)
+
 		record = ReciteRecord(self.book)
-		buffer.set_text(record.list_word())
 
-		textview.set_buffer(buffer)
+		for word in record.words:
+			iter = model.append()
+			model.set(iter,
+				0, word,
+				1, record.dict[word].strip())
 
-		return textview, record
+		listview.set_model(model)
+
+		renderer = gtk.CellRendererText()
+		column = gtk.TreeViewColumn("单词", renderer, text = 0)
+		listview.append_column(column)
+
+		renderer = gtk.CellRendererText()
+		column = gtk.TreeViewColumn("意思", renderer, text = 1)
+		listview.append_column(column)
+
+		return listview, record
 
 if __name__ == "__main__":
 	win = gtk.Window()
@@ -120,7 +155,8 @@ if __name__ == "__main__":
 
 #        vbox = FirstRecite()
         vbox = FirstRecite("/usr/share/reciteword/books/qqssbdc/cykych/ck-kq.bok")
+	vbox.show()
         win.add(vbox)
 
-        win.show_all()
+        win.show()
 	gtk.main()
