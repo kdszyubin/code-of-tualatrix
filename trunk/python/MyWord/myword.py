c648e0b4d784771a5c0b09571495b62d28ab0a09
tualatrix <tualatrix@bcf3a914-dc41-0410-a79b-5dc8c4d4b3d5>
2008-01-02 14:45:19 +0000

    =First Recite

--- trunk/python/MyWord/myword.py
+++ trunk/python/MyWord/myword.py
@@ -5,13 +5,7 @@ import gtk
 import os
 from choosebook import ChooseBook
 from firstrecite import FirstRecite
-
-def show_info(message, title = "提示", parent = None):
-	dialog = gtk.MessageDialog(None, gtk.DIALOG_MODAL, gtk.MESSAGE_INFO, gtk.BUTTONS_OK)
-	dialog.set_title(title)
-	dialog.set_markup(message)
-	dialog.run()
-	dialog.destroy()
+from widgets import show_info
 
 class MyWord(gtk.Window):
 	def __init__(self):
