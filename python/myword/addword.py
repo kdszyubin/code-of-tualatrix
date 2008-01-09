6b90fa0323f71403f5a634e808a463276e66a250
tualatrix <tualatrix@bcf3a914-dc41-0410-a79b-5dc8c4d4b3d5>
2008-01-09 02:51:04 +0000

    =

--- python/myword/addword.py
+++ python/myword/addword.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
 	for book in books:
 		for word in file(book):
 			if word.find("[W]"+sys.argv[1]+"[T]") >= 0:
-				tualatrix = open(os.path.join(os.path.expanduser("~"),".myword/books/txwords-1.bok"), "a")
+				tualatrix = open(os.path.join(os.path.expanduser("~"),".myword/books/txwords-2.bok"), "a")
 				tualatrix.write(word)
 				tualatrix.close()
 				print "正在搜寻第%d本...找到'%s: %s'并已加入生词本" % ((books.index(book) + 1), sys.argv[1], word.split('[W]')[1].split('[T]')[1].split('[M]')[1].strip())
