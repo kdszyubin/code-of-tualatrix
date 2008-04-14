22b089ea4a782cc09343a4c9ede2b53b557eb7e4
tualatrix <tualatrix@bcf3a914-dc41-0410-a79b-5dc8c4d4b3d5>
2008-04-14 10:57:23 +0000

    Ubuntu Tweak 0.3

--- trunk/programs/python/ubuntu-tweak/Gnome.py
+++ trunk/programs/python/ubuntu-tweak/Gnome.py
@@ -40,6 +40,7 @@ class Gnome(gtk.VBox):
 		box = TablePack(_("<b>GNOME Panel and Menu</b>"), [
 			[Factory.create("gconfcheckbutton", _("Confirm before remove panel"), "confirm_panel_remove")],
 			[Factory.create("gconfcheckbutton", _("Complete lockdown of the Panel "), "locked_down")],
+			[Factory.create("gconfcheckbutton", _("Enable panel animations"), "enable_animations")],
 			[Factory.create("gconfcheckbutton", _("Show Input Method menu in the right-click"), "show_input_method_menu")],
 			[Factory.create("gconfcheckbutton", _("Show Unicode Method menu in the right-click"), "show_unicode_menu")],
 			[gtk.Label(_("Notification-daemon popup location")), Factory.create("gconfcombobox", "popup_location", [_("Top Left"), _("Top Right"), _("Bottom Left"), _("Bottom Right")], ["top_left", "top_right", "bottom_left", "bottom_right"])]
